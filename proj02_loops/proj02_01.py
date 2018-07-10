e305d8c3c260c238a490aeabc26cdbb928e2c5d0
hawiab <33238475+hawiab@users.noreply.github.com>
2018-07-10 10:22:11 -0500

    Add files via upload

--- proj02_loops/proj02_01.py
+++ proj02_loops/proj02_01.py
@@ -17,3 +17,38 @@
 # Enter a number to sum, or 0 to indicate you are finished: 10
 # Enter a number to sum, or 0 to indicate you are finished: 0
 #The sum of your numbers is: 21
+
+# sum = 0
+# UserNo = raw_input("Enter a number to sum, or 0 to indicate you are finished: ")
+#
+# while int(UserNo) >= 0:
+#     sum = sum + int(UserNo)
+#     UserNo = raw_input("Enter a number to sum, or 0 to indicate you are finished: ")
+#     if int(UserNo) == 0:
+#         break
+# print sum
+
+
+# loop = 0
+# x = 1
+# sum = 0
+#
+# while loop < 5 and x != 0:
+#     x = int(raw_input("Input a number or 0 when finished"))
+#     loop = loop + 1
+#     sum = sum + x
+# print "The sum of your numbers is " + str( sum) + "!"
+
+# FibNum = raw_input("How many Fibonacci numbers would you like to generate? ")
+# CurrentNum = 1
+# PreviousNum = 0
+# num = "num"
+# for num in range(int(FibNum)):
+#     print CurrentNum
+#     NextNum = int(CurrentNum) + int(PreviousNum)
+#     PreviousNum = int(CurrentNum)
+#     CurrentNum = int(NextNum)
+
+FibNum = raw_input("How many Fibonacci numbers would you like to generate? ")
+CurrentNum = 1
+PreviousNum = 0

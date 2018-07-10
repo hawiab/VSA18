9113c413acdcc37b6ac929e4f2c12ef71d64a8e3
hawiab <33238475+hawiab@users.noreply.github.com>
2018-07-10 13:43:23 -0500

    Add files via upload

--- proj03_guessing_game/proj03.py
+++ proj03_guessing_game/proj03.py
@@ -12,20 +12,25 @@ Keep track of how many guesses the user has taken, and when the game ends, print
 
 """
 import random
-random_number = random.randint(1,9)
 counter = 0
-print ("Im thinking of a number between 1 and 9. Can you guess my number?")
-for counter in range (1, 4):
-    Guess = raw_input("Enter a number, or '0' to end the game: ")
-    if int(Guess) == 0:
-        print("Wimp!")
-        break
-    elif int(Guess) < random_number:
-        print "Your number is too low!"
-    elif int(Guess) > random_number:
-        print "Your number is too high!"
-    elif int(Guess) == random_number:
-        print "Congratulations, you guessed my number! You used ",counter, " guesses"
-    if counter == 3:
-        print("You ran out of guesses! Idiot!")
+play_again = 1
 
+while int(play_again) != 0:
+    if int(play_again) == 1:
+        random_number = random.randint(1, 9)
+        print ("I'm thinking of a number between 1 and 9. Can you guess my number?")
+        for counter in range (1, 4):
+            Guess = raw_input("Enter a number, or '0' to end the game: ")
+            if int(Guess) == 0:
+                print("Wimp! Why are you at VSA?")
+                break
+            elif int(Guess) < random_number:
+                print "Your number is too low!"
+            elif int(Guess) > random_number:
+                print "Your number is too high!"
+            elif int(Guess) == random_number:
+                print "Congratulations, you guessed my number! You used ",counter, " guesses"
+                break
+            if counter == 3:
+                print("You ran out of guesses! Idiot! I thought smart kids went to this camp.")
+    play_again = raw_input("Would you like to play again? 1 for yes, 0 for no. ")

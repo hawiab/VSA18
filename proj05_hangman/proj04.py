fee1bde50847b67f8265bed2869ec57cdce11f3a
hawiab <33238475+hawiab@users.noreply.github.com>
2018-07-11 14:50:15 -0500

    Add files via upload

---
+++ proj05_hangman/proj04.py
@@ -0,0 +1,60 @@
+# coding=utf-8
+# Name:
+# Date:
+
+"""
+proj04
+
+practice with lists
+
+"""
+
+#Part I
+#Take a list, say for example this one:
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#and write a program that prints out all the elements of the list that are less than 5.
+
+for item in a:
+    if item < 30:
+        print item
+
+
+
+
+#Part II
+# Take two lists, say for example these two:
+b = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+# and write a program that creates and prints a list that contains only the elements
+# that are common between the lists (without duplicates).
+# Make sure your program works on two lists of different sizes.
+for items in b:
+    for numbers in c:
+        if items == numbers:
+            print items
+items.append[1]
+
+#Part III
+# Take a list, say for example this one:
+
+d = ["b", "a", "f", "y", "a", "t", "_", "p", "a", "R"]
+# and write a program that replaces all “a” with “*”.
+
+for a in d:
+     with *
+
+
+
+
+#Part IV
+#Ask the user for a string, and print out whether this string is a palindrome or not.
+
+
+
+
+
+
+
+
+

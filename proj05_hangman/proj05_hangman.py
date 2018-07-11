e684f7e1084f6442a69ad2f3aaf4e13c5843c16d
hawiab <33238475+hawiab@users.noreply.github.com>
2018-07-11 14:51:02 -0500

    Add files via upload

--- proj05_hangman/proj05_hangman.py
+++ proj05_hangman/proj05_hangman.py
@@ -46,3 +46,58 @@ def choose_word(wordlist):
 wordlist = load_words()
 
 # your code begins here!
+
+
+
+
+y = ""
+while y != "n" and y != "no":
+    word = choose_word(wordlist)
+    guesses = 8
+    alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    wordlength = len(word)
+
+    print "Welcome to the game, Hangman!"
+    print "I am thinking of a word that is", len(word),"letter(s) long."
+    wordblank = ["_ "] * len(word)
+
+    while True:
+        if wordlength == 0:
+            print "--------------------"
+            print "Congratulations! You won the game! The word was", str(word) + "!"
+            break
+        elif guesses == 0:
+            print "--------------------"
+            print "Good try. The word is",word
+            break
+        else:
+            print "--------------------"
+            counter = 0
+            deleted = 0
+            print "Available Letters:"," ".join(alphabet)
+            print "You have", guesses ,"guess(es) left."
+            letter = raw_input("Please guess a letter: ")
+            if letter in alphabet:
+                    if letter in word:
+                        while counter < len(word):
+                            if letter == word[counter]:
+                                wordlength = wordlength - 1
+                                wordblank[counter] = letter + " "
+                                if deleted == 0:
+                                    alphabet.remove(letter)
+                                    deleted = 1
+                            counter = counter + 1
+                        print "Good Guess:", "".join(wordblank)
+                    else:
+                        print "Oops, that letter isn't in the word:", "".join(wordblank)
+                        guesses = guesses - 1
+            else:
+                print "Guess again: "
+    print "--------------------"
+    while y != "yes" or y != "y" or y != "no" or y != "n":
+        y = raw_input("Do you want to play again? ")
+        if y == "yes" or y == "y" or y == "no" or y == "n":
+            break
+        else:
+            print "Type a valid answer."
+

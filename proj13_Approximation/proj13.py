34ba6ce0ed688c711135660411f43be4888064ca
hawiab <33238475+hawiab@users.noreply.github.com>
2018-07-24 11:40:08 -0500

    Add files via upload

--- proj13_Approximation/proj13.py
+++ proj13_Approximation/proj13.py
@@ -5,7 +5,13 @@
 
 
 # Problem 1: Paying the Minimum
-
+ outBalance = raw_input("Enter the outstanding balance on your credit card: ")
+ yrInterest = raw_input("Enter the annual credit card interest rate as a decimal: ")
+ minPayment = raw_input("Enter the minimum monthly payment rate as a decimal: ")
+ minMonthpay = int(minPayment)
+ interestPaid = int(yrInterest)/(12*int(outBalance))
+ princiPaid = int(minPayment) - int(interestPaid)
+ remainBal = int(outBalance) - int(princiPaid)
 # Problem 2: Paying Debt Off in a Year
 
 # Problem 3: Use bisection search to solve faster
